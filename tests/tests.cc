35dc436df752b26b41dbf612bc150b657a8ab2f0
Uquoon <qiyukun64@gmail.com>
2025-10-03 15:34:22 +0000

    check

--- tests/tests.cc
+++ tests/tests.cc
@@ -74,3 +74,7 @@ TEST_CASE("Example: Print Prompt Ledger", "[ex-3]") {
   atm.PrintLedger("./prompt.txt", 12345678, 1234);
   REQUIRE(CompareFiles("./ex-1.txt", "./prompt.txt"));
 }
+
+TEST_CASE("", ""){
+  
+}
